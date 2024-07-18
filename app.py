f7acf75d2b9c5100ab937cb8b9cfa308698c956d
Samit Kumar Patel <samitkumarpatel@users.noreply.github.com>
2024-07-18 22:59:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def get_containers_by_node(node_id):
     return jsonify(containers_info)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
